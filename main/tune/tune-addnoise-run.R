d4da8b14b785cb122a0bfa798986586a7ea27f98
Sangwon Hyun <shyun@cmu.edu>
2018-06-05 15:34:33 -0400

    Added script for addnoise simulations

---
+++ main/tune/tune-addnoise-run.R
@@ -0,0 +1,17 @@
+## Synopsis: this script is to be run on command line as "Rscript
+## ../main/tune-addnoise-run.R" from binseginf/binseginf directory,
+
+library(binseginf)
+source("../main/tune/tune-addnoise.R")
+
+sigma.add.list = seq(from=0, to=2, length=10)
+nsim = 2000
+## ## args = commandArgs(trailingOnly=TRUE)
+## ## ii.list = as.numeric(args)
+## ii.list = 1
+## ii.list=10
+for(ii in ii.list){
+    sigma.add = sigma.add.list[ii]
+    dosim(sigma.add=sigma.add,
+          nsim=nsim, mc.cores=8)
+}
