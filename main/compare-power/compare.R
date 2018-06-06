36147d8bab0a2d542bc3c2256a2af197f376fb96
Sangwon Hyun <shyun@cmu.edu>
2018-06-06 16:36:20 -0400

    Made accompanying changes to compare-power simulation script

--- main/compare-power/compare.R
+++ main/compare-power/compare.R
@@ -1,19 +1,19 @@
 ## Synopsis: Simulation code to compare each method's power. To be run from compare-run.R
-dosim <- function(lev, nsim, mc.cores=1){
+dosim <- function(lev, nsim, n=200, meanfun=fourjump, mc.cores=1, numSteps=4, filename=NULL){
     cat("lev=", lev, fill=TRUE)
     outputdir = "../output"
     onesim <- function(isim){
 
         ## Generate data
-        n = 200
-        mn = fourjump(lev=lev, n=n)
+        ## n = 200
+        mn = meanfun(lev=lev, n=n)
         y = mn + rnorm(n, 0, 1)
         results = list()
 
 
         ## Plain BS inference
         tryCatch({
-        obj = bsfs(y, numSteps=4)
+        obj = bsfs(y, numSteps=numSteps)
         obj = addpv(obj, sigma=1, mn=mn)
         results$bsfs_plain= obj$pvs
         results$bsfs_plain_zero = (obj$means==0)
@@ -21,7 +21,7 @@ dosim <- function(lev, nsim, mc.cores=1){
         
         ## Noisy BS inference
         tryCatch({
-        obj = bsfs(y, numSteps=4, sigma.add=0.2)
+        obj = bsfs(y, numSteps=numSteps, sigma.add=0.2)
         obj = addpv(obj, sigma=1, sigma.add=0.2, type="addnoise", mn=mn)
         results$bsfs_addnoise = obj$pvs
         results$bsfs_addnoise_zero = (obj$means==0)
@@ -29,7 +29,7 @@ dosim <- function(lev, nsim, mc.cores=1){
         
         ## Plain WBS inference
         tryCatch({
-        obj = wbsfs(y, numSteps=4, numIntervals=length(y))
+        obj = wbsfs(y, numSteps=numSteps, numIntervals=length(y))
         obj = addpv(obj, sigma=1, type="plain", mn=mn)
         results$wbsfs_plain = obj$pvs
         results$wbsfs_plain_zero = (obj$means==0)
@@ -37,7 +37,7 @@ dosim <- function(lev, nsim, mc.cores=1){
         
         ## Marginalized WBS inference
         tryCatch({
-        obj = wbsfs(y, numSteps=4, numIntervals=length(y))
+        obj = wbsfs(y, numSteps=numSteps, numIntervals=length(y))
         obj = addpv(obj, sigma=1, type="rand", mn=mn)
         results$wbsfs_marg = obj$pvs
         results$wbsfs_marg_zero = (obj$means==0)
@@ -45,7 +45,7 @@ dosim <- function(lev, nsim, mc.cores=1){
         
         ## Plain CBS inference
         tryCatch({
-        obj = cbsfs(y, numSteps=2)
+        obj = cbsfs(y, numSteps=numSteps/2)
         obj = addpv(obj, sigma=1, type="plain", mn=mn)
         results$cbsfs_plain = obj$pvs
         results$cbsfs_plain_zero = (obj$means==0)
@@ -53,7 +53,7 @@ dosim <- function(lev, nsim, mc.cores=1){
         
         ## Noisy CBS inference
         tryCatch({
-        obj = cbsfs(y, numSteps=2, sigma.add=0.2)
+        obj = cbsfs(y, numSteps=numSteps/2, sigma.add=0.2)
         obj = addpv(obj, sigma=1, type="addnoise", mn=mn, sigma.add=0.2)
         results$cbsfs_addnoise = obj$pvs
         results$cbsfs_addnoise_zero = (obj$means==0)
@@ -61,7 +61,7 @@ dosim <- function(lev, nsim, mc.cores=1){
         
         ## Plain FL inference 
         tryCatch({
-        obj = fl(y, numSteps=4)
+        obj = fl(y, numSteps=numSteps)
         obj = addpv_fl(obj, sigma=1, type="plain", mn=mn)
         results$fl_plain = obj$pvs
         results$fl_plain_zero = (obj$means==0)
@@ -69,7 +69,7 @@ dosim <- function(lev, nsim, mc.cores=1){
         
         ## Noisy FL inference
         tryCatch({
-        obj = fl(y, numSteps=1, sigma.add=0.2)
+        obj = fl(y, numSteps=numSteps, sigma.add=0.2) 
         obj = addpv_fl(obj, sigma=1, sigma.add=0.1, type="addnoise", mn=mn)
         results$fl_addnoise = obj$pvs
         results$fl_addnoise_zero = (obj$means==0)
@@ -87,7 +87,7 @@ dosim <- function(lev, nsim, mc.cores=1){
         onesim(isim)
     }, mc.cores=mc.cores, mc.preschedule=TRUE) ## If you use mc.preschedule=FALSE, it will use different cores!
 
-    filename = paste0("compare-power-lev", lev, ".Rdata")
+    if(!is.null(filename)) filename = paste0("compare-power-lev", lev, ".Rdata")
     save(results.list, file=file.path(outputdir, filename))
     ## return(results.list)
 }
