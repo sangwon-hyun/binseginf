85c05df7ef6253c9939f1b4d107419e9fe23eaff
Sangwon Hyun <shyun@cmu.edu>
2018-08-09 05:54:14 -0400

    Modified compare-run script to work with multiple steps

--- main/compare-power/compare-run.R
+++ main/compare-power/compare-run.R
@@ -1,23 +1,25 @@
 ## Synopsis: Script to run the entire set of experiments. Run by ``Rscript
-## ../main/compare-power/compare-run.R 1 2 3 4 5 6 7 8 9 10 11 12'', from the directory
+## ../main/compare-power/compare-run.R bsfs'', from the directory
 ## binseginf/binseginf
 
 ## Setting
 library(binseginf)
 source("../main/compare-power/compare.R")
-levs = c(0, 0.2, 0.4, 0.6, 0.8, 1, 1.5, 2, 2.5, 3, 3.5, 4)
+levs = c(0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4)
 nsims = c(3000,3000,3000, seq(from=3000,to=1000,length=5),
-            round(seq(from=600, to=300, length=4) ))
+          round(seq(from=600, to=300, length=4) ))
 
 args = commandArgs(trailingOnly=TRUE)
-ii.list = as.numeric(args)
-nchunk=10
+## ii.list = as.numeric(args)
+type = args
+print(type)
+
+ii.list = 1:length(levs)
+nchunk = 10
 for(ii in ii.list){
     lev = levs[ii]
     nsim = nsims[ii]
     for(ichunk in 1:nchunk){
-        dosim(lev=lev, ichunk=ichunk, nsim=nsim/nchunk, mc.cores=8)
+        dosim(lev=lev, ichunk=ichunk, nsim=nsim/nchunk, mc.cores=8, type=type)
     }
 }
-
-
